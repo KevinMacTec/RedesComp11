4cbf657681a048e384763200368213ae3f1c9b43
KevinMacTec <kevinmactec@gmail.com>
2024-08-26 17:36:25 -0300

    clase server, programa cliente agregado, faltan cosas

--- redes2024_ob1/part2/jsonrpc_redes/client.py
+++ redes2024_ob1/part2/jsonrpc_redes/client.py
@@ -1,6 +1,23 @@
 #cliente
 import client_server
+import socket
 
-def connect(adress, port):
-    conn=0
-    return conn
+client_socket = None
+servers = []
+        
+def client_sending(master,message_to_send):
+    client_server.send_all(master,message_to_send)
+    message_received = ""
+    while message_received.pos("SUCCESS\n") > 0:
+        message_received_buffer = b""
+        message_received_buffer = master.recv()
+        message_received_buffer.decode("utf-8")
+        message_received += message_received_buffer
+
+#realiza la coneccion con el servidor
+def connect(address):
+    serverIP, serverPort = address
+    master = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    master.connect((serverIP, serverPort))
+    master.settimeout(None)
+    return master
--- redes2024_ob1/part2/jsonrpc_redes/server.py
+++ redes2024_ob1/part2/jsonrpc_redes/server.py
@@ -1,53 +1,66 @@
 #servidor
-from jsonrpc_redes import client_server
+import client_server
+import threading
 import socket
 
-funciones_implementadas = ["sum","echo","echo_concat"]
+class Server:
+    def __init__(self, host, port):
+        self._host = host
+        self._port = port
+        self._methods = {}
+        self._clients = []
+        self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._server_socket.bind((self.host, self.port))
+        self._server_socket.listen(1)
+        print(f"Servidor iniciado en {self.host}:{self.port}")
 
-def sum(*args):
-    total = 0
-    for number in args:
-        total += number
-    return total
+    def print(self):
+        print("Servidor ", self.host,":",self.port)
 
-def echo(mensaje):
-    return mensaje
+    def add_method(self, *args):
+        if not args:
+            raise ValueError("Debe proporcionar al menos el nombre del metodo.")
+        
+        function_name = args[0]
+        parameters = args[1:]
+        self.methods[function_name] = parameters
 
-def echo_concat(*args):
-    total = ""
-    for mensaje in args:
-        total += mensaje
-    return total
+    def shutdown(self):
+        self.server_socket.close()
+        self.print()
+        print(", cerro.")
 
+    def serve(self):
+        while True:
+            try:
+                client_socket, client_address = self._server_socket.accept()
+                print("Conexión establecida con ", client_address)
+                thread = threading.Thread(target=self.accept_connection, args=client_socket)
+                thread.start()
+            except Exception as err:
+                print("Error al conectar ")
+                self.print()
+                print(" con ")
+                print(client_address)
 
-#para recibir una solicitud como retornar parametros,
-#nos tenemos que poner deacuerdo en el formato de
-#envio y recepción de los Json
-def retornar_parametros(*args):
-    parametros = []
-    while (args.substr("\n")>0): #asumo que se retiran parametros cuando aún queda el \n
-        l, err = read_line(skt) #recibe el string del Json
-        while (args.substr(",")>0): #separa el string de args en una lista de parametros
-            parametros.insert(args)
+    def accept_connection(self, client_socket):
+        try:
+            #hay que ver como se comunica entre servidor y cliente
+            conect = False
+        except Exception:
+            client_socket.close()
 
-    return parametros
+    def server_sending(client,message_to_send):
+        client_server.send_all(client,message_to_send)
 
-def recibir_solicitud(skt):
-    while (True):
-        l, err = read_line(skt) #recibe el string del Json
-        if err == "closed":
-            return l, "closed"
-        for funciones in funciones_implementadas:
-            if l.substr(funciones)>0:
-                nombre_funcion = l.sub(1,funciones.len())
-                resto_l = l.sub(funciones.len()+2, l.len())
-                funcion = locals().get(nombre_funcion)
-                if callable(funcion):
-                    parametros = retornar_parametros()
-                    funcion(*parametros) #realiza la funcion
-                    return
-                else:
-                    return # deberia de retornar error pero luego vemos como hacemos
+    def connect(self,skt):
+        while True:
+            try:
+                client, _ = skt.accept()
+                thread = threading.Thread(target=self.accept_connection, args=client)
+                thread.start()
+            except Exception:
+                print("Error al conectar con TCP")
 
 
         
